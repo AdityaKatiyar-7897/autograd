a14d097ed4feb4c6332014d50130f167dc0c01e5
Aditya Katiyar <rishkat1249@gmail.com>
2026-06-08 16:06:16 +0530

    Creating soemthing like a value object and trying on to values a and b and print its values

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
 typedef struct {
-	float data;
+    float data;
+    float grad;
 } Value;
+
+Value value_create(float data)
+{
+    Value v;
+
+    v.data = data;
+    v.grad = 0.0f;
+
+    return v;
+}
 
 int main(void)
 {
-	Value a;
+    Value a = value_create(2.0f);
+    Value b = value_create(3.0f);
 
-	a.data = 2.0f;
+    printf("a = %.2f\n", a.data);
+    printf("b = %.2f\n", b.data);
 
-	printf("a.data = %f\n", a.data);
-
-	return 0;
+    return 0;
 }
