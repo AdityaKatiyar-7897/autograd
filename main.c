49a1afd988df7b8d122dd0272244f831f0d6dc30
Aditya Katiyar <rishkat1249@gmail.com>
2026-06-08 16:56:01 +0530

    Building smallest possible graph

--- main.c
+++ main.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
 
-typedef struct {
+typedef struct Value {
+
     float data;
     float grad;
+
+    struct Value *left;
+    struct Value *right;
+
 } Value;
 
-Value value_create(float data)
+int main()
 {
-    Value v;
+    Value a;
+    Value b;
+    Value c;
 
-    v.data = data;
-    v.grad = 0.0f;
+    a.data = 2.0f;
+    a.grad = 0.0f;
+    a.left = NULL;
+    a.right = NULL;
 
-    return v;
-}
+    b.data = 3.0f;
+    b.grad = 0.0f;
+    b.left = NULL;
+    b.right = NULL;
 
-int main(void)
-{
-    Value a = value_create(2.0f);
-    Value b = value_create(3.0f);
+    c.data = a.data * b.data;
+    c.grad = 0.0f;
 
-    printf("a = %.2f\n", a.data);
-    printf("b = %.2f\n", b.data);
+    c.left = &a;
+    c.right = &b;
+
+    printf("Address of a      : %p\n", (void*)&a);
+    printf("Stored in c.left  : %p\n\n", (void*)c.left);
+
+    printf("Address of b      : %p\n", (void*)&b);
+    printf("Stored in c.right : %p\n\n", (void*)c.right);
+
+    printf("c.data = %.2f\n", c.data);
 
     return 0;
 }
